082d1064654504abc88c2760840f3ce75296b3a2
SCARLETT SWERDLOW <scarlettswerdlow@SCARLETTs-MacBook.local>
2022-08-30 09:02:13 -0400

    Created config file

---
+++ src/make_text.py
@@ -0,0 +1,14 @@
+import yaml
+
+#########################
+#                       #
+#       FUNCTIONS       #
+#                       #
+#########################
+
+def read_yaml(file_path):
+
+    with open(file_path, "r") as f:
+        return yaml.safe_load(f)
+
+
